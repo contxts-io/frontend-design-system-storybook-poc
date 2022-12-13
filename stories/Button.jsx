b10e50d0e2da42ebff41a98bba7455edbb59f75d
tina kang <tina@contxts.io>
2022-12-13 15:34:37 +0900

    feat - 예시 버튼 추가

--- stories/Button.jsx
+++ stories/Button.jsx
@@ -1,50 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './button.css';
+import { MainButtonStyle } from './button.styled';
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const MainButton = (props) => {
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
+    <MainButtonStyle {...props}>
+      {props.label}
+    </MainButtonStyle>
   );
 };
 
-Button.propTypes = {
+MainButton.propTypes = {
   /**
-   * Is this the principal call to action on the page?
+   * 버튼 모양입니다.
    */
-  primary: PropTypes.bool,
+  variant: PropTypes.oneOf(['primary', 'secondary', 'tertiary']),
   /**
-   * What background color to use
+   * 버튼 내부 텍스트입니다.
    */
-  backgroundColor: PropTypes.string,
+  label: PropTypes.string,
   /**
-   * How large should the button be?
+   * 버튼 가로 너비입니다. `100%` 는 화면에 가득 찹니다. 
+   * 특정 너비를 지정하고 싶다면 `100px` 와 같이 지정합니다.
+   */
+  width: PropTypes.string,
+  /**
+   * `True` 를 지정하면 조작 불가 상태의 UI를 확인할 수 있습니다.
+   */
+  disabled: PropTypes.bool,
+  /**
+   * `True` 를 지정하면 로딩중 상태의 UI를 확인할 수 있습니다.
+   */
+  loading: PropTypes.bool,
+  /**
+   * 버튼 사이즈입니다.
    */
   size: PropTypes.oneOf(['small', 'medium', 'large']),
   /**
-   * Button contents
-   */
-  label: PropTypes.string.isRequired,
-  /**
-   * Optional click handler
+   * (Optional) 클릭하면 발생하는 콜백 함수입니다.
    */
   onClick: PropTypes.func,
+  /**
+   * (Optional) 배경색을 자유롭게 지정할 수 있습니다.
+   */
+  backgroundColor: PropTypes.string,
 };
 
-Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
+MainButton.defaultProps = {
+  width: '100%',
+  disabled: false,
+  loading: false,
   size: 'medium',
-  onClick: undefined,
 };
--- stories/Button.stories.jsx
+++ stories/Button.stories.jsx
@@ -1,24 +1,58 @@
 import React from 'react';
 
-import { Button } from './Button';
+import { MainButton } from './Button';
+import { ButtonWrapper } from './button.styled';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
-  component: Button,
+  component: MainButton,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
+    variant: {
+      options: ['primary', 'secondary', 'tertiary'],
+      control: { type: 'select' },
+    },
+    size: { control: 'select' },
     backgroundColor: { control: 'color' },
+    label: { control: 'text' },
+    width: { control: 'text' },
+  },
+  parameters: {
+    componentSubtitle:
+      'NFTBank에서 사용되는 버튼입니다. 버튼의 크기, 색상, 너비 등을 조절할 수 있습니다.',
   },
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <Button {...args} />;
-
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
+export const Standard = (args) => <MainButton {...args} />
+Standard.args = {
+  variant: 'primary',
+  width: '100%',
+  label: 'Select',
+  disabled: false,
+  loading: false,
+  size: 'medium',
 };
 
+// Other Button stories
+
+export const Variants = (args) => (
+  <ButtonWrapper>
+    <MainButton {...args} variant="primary" />
+    <MainButton {...args} variant="secondary" />
+    <MainButton {...args} variant="tertiary" />
+  </ButtonWrapper>
+);
+Variants.args = {
+  width: '200px',
+  label: 'Select',
+  disabled: false,
+  loading: false,
+  size: 'medium',
+};
+Variants.parameters = {
+  docs: {
+    storyDescription: '3가지 타입의 variant를 지원합니다.',
+  },
+};
